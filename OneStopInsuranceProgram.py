c3ce88b7430052237411ee6fd35d5704dd7989ee
Justin Seaward <jut_seaward@yahoo.com>
2025-07-31 22:20:04 -0230

    Inputs and validation complete. Calculations and if statement entered. Proof reading and testing started.

--- OneStopInsuranceProgram.py
+++ OneStopInsuranceProgram.py
@@ -7,34 +7,35 @@
 import datetime
 import os
 import sys
-import FormatValues
+import FormatValues as FV
 
 # Define program constants.
-f = open("Defults.dat", "r") # Defaults.dat is the file where the values for the constants are stored in. 
+f = open("Defaults.dat", "r") # Defaults.dat is the file where the values for the constants are stored in. 
 
 POL_NUM = int(f.readline()) # Policy number starts at 1944.
-BAS_PREM_COST = int(f.readline()) # Basic premium cost is $869.00.
+BASIC_PREM_COST = float(f.readline()) # Basic premium cost is $869.00.
 DIS_CAR_ESP = float(f.readline()) # Discount for additional car(s) is 25%.
-EXT_LIB_COST = int(f.readline()) # The cost of extra liability coverage is $130.00.
-GLASS_COST = int(f.readline()) # The cost for glass coverage is $86.00.
-LOANER_COV_COST = int(f.readline()) # The cost for the loaner car coverage $58.00.
+EXT_LIA_COST = float(f.readline()) # The cost of extra liability coverage is $130.00.
+GLASS_COST = float(f.readline()) # The cost for glass coverage is $86.00.
+LOANER_CAR_COST = float(f.readline()) # The cost for the loaner car coverage $58.00.
 HST_ESP = float(f.readline()) # HST rate is 15%.
 PRO_FEE_COST = float(f.readline()) # Processing fee for the monthly payments is $39.99.
 
 f.close
 
+CURR_DATE = datetime.datetime.now() # Current date.
+
 # Define program fucntions.
 
 # Main program starts here.
     
-
 while True:
 
     os.system("cls" if os.name == "nt" else "clear") # Clears the screen when program is lanched.
-
     # Gather user inputs.
+    
     while True:
-    # Input and validations for customer first name.
+        # Input and validation(s) for customer first name.
         print()
         CustFN = input("Enter the customers first name: ").title()
         if CustFN == "":
@@ -47,23 +48,351 @@ while True:
             print()
         else:
             break
-
-        while True:
-            # Input and validations for customer last name.
+        
+        #print(CustFN) # Test print.
+        
+    while True:
+        # Input and validation(s) for customer last name.
+        print()
+        CustLN = input("Enter the customers last name: ").title()
+        if CustLN == "":
             print()
-            CustLN = input("Enter the customers last name: ").title()
-            if CustLN == "":
+            print("    Data Entry Error - Customer last name cannot be blank.")
+            print()
+        elif CustLN.isalpha() == False:
+            print()
+            print("    Data Entry Error - Invalid characters, must contain letters only.")
+            print()
+        else:
+            break
+        
+        #print(CustLN) # Test print.
+
+    while True:
+        # Input and validation(s) for street address.
+        print()
+        StAdd = input("Enter the street address: ")
+        if StAdd == "":
+            print()
+            print("    Data Entry Error - Street address cannot be blank.")
+            print()
+        else:
+            break
+    
+        #print(StAdd) # Test print.
+
+    while True:
+        # Input and validation(s) for city.
+        print()
+        City = input("Enter the city: ").title
+        if StAdd == "":
+            print()
+            print("    Data Entry Error - City cannot be blank.")
+            print()
+        else:
+            break
+        
+        #print(City) # Test print.
+
+    while True:
+        # Input and validation(s) for the postal code.
+        print()
+        PostalCode = input("Enter the postalcode (A9A9A9): ")
+        if PostalCode == "":
+            print()
+            print("    Data Entry Error - Postalcode cannot be blank.")
+            print()
+        elif len(PostalCode) != 6:
+            print()
+            print("   Data Entry Error - Postalcode must be 6 characters.")
+            print()    
+        else:
+            break
+        ###########
+        # Not working from line 108 to 116.
+        '''
+        elif PostalCode[0].isalpha == False or PostalCode[2].isalpha == False or PostalCode[4].isalpha == False:
+            print()
+            print("   Data Entry Error - Postalcode must conatin three letters and three numbers (A9A9A9).")
+            print()
+        elif PostalCode[1].isdigit == False or PostalCode[3].isdigit == False or PostalCode[5].isdigit == False:
+            print()
+            print("   Data Entry Error - Postalcode must conatin three letters and three numbers (A9A9A9).")
+            print()
+        ###########
+        '''
+    
+        
+        print(PostalCode) # Test print.
+
+    ProvLst = ["NL", "NS", "PEI", "NB", "QE", "ON", "MB", "SK", "AB", "BC", "NWT", "YT", "NT"] # Canadian province list to compare agaisnt user input.
+
+    while True:
+        # Input and validation for the province.
+        Prov = input("Enter the customer province (XX): ").upper()
+
+        if Prov == "":
+            print()
+            print(" Data entry Error - The province cannot be blank.")
+            print()
+        elif len(Prov) != 2:
+            print()
+            print("Data entry error - The province must be two(2) charaters.")
+            print()
+        elif Prov not in ProvLst:
+            print()
+            print("Data entry error - The province entered is invalid." )
+            print()
+        else:
+            break
+
+        #print(Prov) # Test print.   
+
+    while True:
+    # Input and validations for phone number.
+        try:
+            print()
+            PhoNum = input("Enter the phone number (9999999999): ")
+            if PhoNum == "":
                 print()
-                print("    Data Entry Error - Customer last name cannot be blank.")
+                print("    Data Entry Error - Phone number cannot be blank.")
                 print()
-            elif CustLN.isalpha() == False:
+                continue
+        except ValueError:
+            print()
+            print("    Data Entry Error - Phone number contains invalid characters.")
+            print()
+        else:
+            if len(PhoNum) != 10:
                 print()
-                print("    Data Entry Error - Invalid characters, must contain letters only.")
+                print("    Data Entry Error - Phone number has to contain 10 characters.")
                 print()
             else:
                 break
-    
-        
-            
+           
+        #print(PhoNum) #test print.
+       
+    while True:
+        # Input and validation(s) for the number of cars to be insured.
+        print()
+        try:
+            NumCarsInsure = input("Enter the number of cars to be insured: ")
+            NumCarsInsure = int(NumCarsInsure)
+        except ValueError:
+            print()
+            print("    Data Entry Error - Number of cars to be insured contains invalid characters.")
+            print()
+        else:
+            break
 
+        print(NumCarsInsure) # Test print.
+
+    while True:
+        # Input and validation(s) for extra liability uo to #1,000,000.
+        print()
+        ExtLia = input("Would you like extra liability up to $1,000,000 (Y or N): ").upper() 
+        if ExtLia == "":
+            print()
+            print("    Data Entry Error - Extra liability cannot be blank.")
+            print()
+        elif ExtLia != "Y" and ExtLia != "N":
+            print()
+            print("    Data Entry Error - Extra liability must be entered as Y or N.")
+            print()
+        else:
+            break
+
+        print(ExtLia) # Test print.
+       
+    while True:
+        # Input and validation(s) for glass coverage.
+        print()
+        GlassCover = input("Would you like glass coverage (Y or N): ").upper() 
+        if GlassCover == "":
+            print()
+            print("    Data entry error - Glass coverage cannot be blank.")
+            print()
+        elif GlassCover != "Y" and GlassCover != "N":
+            print()
+            print("    Data entry error - Glass coverage must be entered as Y or N.")
+            print()
+        else:
+            break
+
+        print(GlassCover) # Test print.
+       
+    while True:
+        # Input and validation(s) for the optional loaner car coverage.
+        print()
+        LoanCar= input("Would you like to add a loaner car (Y or N): ").upper() 
+        if LoanCar == "":
+            print()
+            print("    Data entry error - Loaner car cannot be blank.")
+            print()
+        elif LoanCar != "Y" and LoanCar != "N":
+            print()
+            print("    Data entry error - Loaner car must be entered as Y or N.")
+            print()
+        else:
+            break
+
+        print(LoanCar) # Test print.
+
+    PayMethodLst =["Full", "Monthly", "Downpay"]
+    DownPayAmt = 0
+    while True:
+        # Input and validation for the payment type.
+        print()
+        PayMethod = input("Enter the payment method (Full, Monthly, Downpay): ").title()
+        if PayMethod == "":
+            print()
+            print(" Data entry error - The payment method cannot be blank.")
+            print()
+        elif PayMethod not in PayMethodLst:
+            print()
+            print("Data entry error - The payment method entered is invalid, must be Full, Monthly, or DownPay" )
+            print()    
+        else:
+            if PayMethod == "Monthly" or PayMethod == "Downpay":
+                print()
+                DownPayAmt = input("Enter the down payment amount: ")
+                DownPayAmt = float(DownPayAmt)
+            else:
+                break
+        
+        print(PayMethod) # Test print.
+        print(DownPayAmt) # Test print.
+        
+    while True:
+        # Input and validation for the claim number.
+        print()
+        try:
+            ClaimNum = input("Enter the claim number: ")
+            if ClaimNum == "":
+                print()
+                print(" Data entry error - The claim number cannot be blank.")
+                print()
+                continue
+            elif len(ClaimNum) != 5:
+                print()
+                print(" Data entry error - The claim number has to be 5 characters.")
+                print()
+                continue
+            ClaimNum = int(ClaimNum)
+        except ValueError:
+                print()
+                print(" Data entry error - The claim number contains invalid characters.")
+                print()
+        else:
+            break
+
+        print(ClaimNum) # Test print.
+
+    while True:
+        # Input and validation for the claim date.
+        print()
+        try:
+            ClaimDate = input("Enter the claim date: ")
+            ClaimDate = datetime.datetime.strptime(ClaimDate, "%Y-%m-%d")
+            if ClaimDate == "":
+                print()
+                print(" Data entry error - Claim datew cannot be blank.")
+                print()
+        except ValueError:
+                print()
+                print(" Data entry error - Claim date must be entered YYYY-MM-DD.")
+                print()
+        else:
+            # Check that the date does not exceed the current date.
+            if ClaimDate > CURR_DATE:
+                print() 
+                print(" Data entry error - Claim date can not exceed the current date.")
+                print()
+                continue
+            else:    
+                break
+        
+        print(ClaimDate) # Test print.
+
+    while True:
+        # Input and validation for the claim amount.
+        print()
+        try:
+            ClaimAmt = input("Enter the claim amount: ")
+            ClaimAmt = float(ClaimAmt)
+            if ClaimAmt == "":
+                print()
+                print(" Data entry error - The claim amount cannot be blank.")
+                print()
+                continue
+        except ValueError:
+                print()
+                print(" Data entry error - The claim amount contains invalid characters.")
+                print()
+        else:
+            break    
+
+        print(ClaimAmt) # Test print.
+
+
+    # Define program calculations
+
+    # Calculation for insurance premium.
+    if NumCarsInsure == 1:
+        InsureCost = BASIC_PREM_COST
+    elif NumCarsInsure > 1:
+        NumCarsInsure -= 1
+        InsureCost = (BASIC_PREM_COST - (DIS_CAR_ESP * BASIC_PREM_COST)) * NumCarsInsure
+        NumCarsInsure += 1
+        
+    # Calculation for total extra cost.
+    # Option for extra liability
+    if ExtLia == "Y":
+        ExtLiaCost = EXT_LIA_COST * NumCarsInsure
+        ExtLia == "Yes"
+    else: 
+        ExtLia == "N"
+        ExtLiaCost = 0 
+        ExtLia = "No"
+
+    # Option for glass coverage.
+    if  GlassCover == "Y":
+        GlassCoverCost = GLASS_COST * NumCarsInsure
+        GlassCover == "Yes"
+    else: 
+        GlassCover == "N"
+        GlassCoverCost = 0 
+        GlassCover = "No"
+    
+    # Option for loaner car.
+    if LoanCar == "Y":
+        LoanCarCost = LOANER_CAR_COST * NumCarsInsure
+        LoanCar == "Yes"
+    else: 
+        LoanCar == "N"
+        LoanCarCost = 0 
+        LoanCar = "No"
+    
+    TotExtCosts = EXT_LIA_COST + GLASS_COST + LOANER_CAR_COST
+
+    # Calculation for total insurance premium:
+    TotInsurPrem = InsureCost + TotExtCosts
+
+    # Calculation for total cost:
+    TotCost = TotInsurPrem * (HST_ESP + 1)
+
+    # If statement and calculation for 8 monthly payments with down payment and without down payment:
+    if PayMethod == "Downpay":
+        MonPay = ((TotCost + PRO_FEE_COST) - DownPayAmt) / 8
+    elif PayMethod == "Monthly":
+        MonPay = (TotCost + PRO_FEE_COST) / 8
+    
+
+    Wait = input("Press enter key to continue or enter End: ").title()
+    if Wait == "End":
+        break
+    if Wait == "":
+        continue
+
+'''
 # Any housekeeping duties at the end of the program.
